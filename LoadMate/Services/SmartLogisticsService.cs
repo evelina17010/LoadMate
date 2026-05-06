007d8be861656e05d97453ab477e0129ee2b24eb
arkad <arkad@ARKADIY>
2026-05-06 22:32:28 +0300

    555

--- LoadMate/Services/SmartLogisticsService.cs
+++ LoadMate/Services/SmartLogisticsService.cs
@@ -12,16 +12,14 @@ namespace LoadMate.Services
         public List<Truck> GetAvailableTrucksForCargo(Cargo cargo, List<Truck> allTrucks)
         {
             if (cargo == null) return new List<Truck>();
-            var destinationCity = cargo.Order?.FirstOrDefault()?.Route?.Address1?.Street?.City?.Name;
 
-            return allTrucks.Where(t =>
-                t.TruckStatus_id == 1 && 
-                t.CanFit(cargo) &&       
-                (!t.Order.Any(o => o.OrderStatus_id != 4) || t.Order.Any(o => o.OrderStatus_id != 4 && o.Route?.Address1?.Street?.City?.Name == destinationCity)
-                )
-            )
-            .OrderByDescending(t => t.FreeWeight_kg) 
-            .ToList();
+            var destinationCity = cargo.Order?
+                .FirstOrDefault()?
+                .Route?.Address1?.Street?.City?.Name;
+            return allTrucks
+                .Where(t => t.TruckStatus_id == 1 && t.CanFit(cargo) && (t.Order.Count(o => o.OrderStatus_id != 4) == 0 || 
+                             t.Order.Any(o => o.OrderStatus_id != 4 && o.Route?.Address1?.Street?.City?.Name == destinationCity))) 
+                .OrderByDescending(t => t.FreeWeight_kg).ToList();
         }
     }
 }
